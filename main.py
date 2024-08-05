a1ad73a5b7a3b4d4bf66e54f4f744db87ff2a4ab
choonsam22 <148427160+choonsam22@users.noreply.github.com>
2024-08-05 15:48:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 import streamlit as st
 import pdfplumber
 from langchain.chat_models import ChatOpenAI
-from dotenv import load_env
 
 # 환경 변수 로드
 os.environ["OPENAI_API_KEY"] = "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"
